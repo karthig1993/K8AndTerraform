701097c9353cfdeb846cddb2b7eb0e1065142205
Karthigesan Abirajan <40916297+karthig1993@users.noreply.github.com>
2025-12-16 12:37:54 +0800

    Adding variables template for simple deployments

---
+++ Terraform/central-location.tf
@@ -0,0 +1,19 @@
+variable "vpn_ip" {
+  default = "101.20.30.50/32"
+}
+
+variable "app_port" {
+  default = "8080"
+}
+
+variable "ssh_port" {
+  default = "22"
+}
+
+variable "ftp_port" {
+  default = "21"
+}
+
+variable "protocol" {
+  default = "tcp"
+}
